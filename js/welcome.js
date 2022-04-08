9e975e05d96b8f7be5d1f8a7d8f51480bc25128b
MazzGuille <mazzcode@gmail.com>
2022-04-08 15:48:55 -0300

    99% ready, only missing links for external proyect

--- js/welcome.js
+++ js/welcome.js
@@ -2,7 +2,8 @@ document.getElementById('name').innerHTML = localStorage.getItem('textvalue');
 
 const tl = gsap.timeline({defaults: {duration: 2}});
 tl
+.from('.logo-container', {rotation: 720, opacity: 0})
 .from('.img-container', {rotation: 720, opacity: 0})
 .from('.resposive-font-h2', {opacity: 0})
-.from('.resposive-font-h1', {scale: 0})
+.fromTo('.resposive-font-h1', {scale: 0}, {scale: 1.5})
 .from('.responsive-footer', {opacity: 0, y: -100});
