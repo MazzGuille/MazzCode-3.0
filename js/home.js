073db23749a732275f2f71010dee8453aac33469
MazzGuille <mazzcode@gmail.com>
2022-04-07 21:50:24 -0300

    home page ready

--- js/home.js
+++ js/home.js
@@ -8,9 +8,9 @@ function welcome() {
 
 const home = gsap.timeline({ defaults: { duration: 2 } });
 home
-.from('.img-container', { opacity: 0, x: 300 })
-.from('.visitor-name', { opacity: 0, y: -300})
+.from('.logo-container', { opacity: 0, x: 300 })
+.from('.name', { opacity: 0, y: -300})
 .from('.btn', { opacity: 0, y: -300}, "-=1"  )
-.from('.footer', { opacity: 0 });
+.from('.responsive-footer', { opacity: 0 });
 
 
